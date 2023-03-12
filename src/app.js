adaefaf62b0f889798251e9a241f06b36e8d8caf
SamihOuague <ouaguenouni.samih@gmail.com>
2023-03-12 09:28:50 +0100

    Cookie message update

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 const humburger = document.getElementById("hamburger");
 const navList = document.getElementById("nav-list");
+const closeBtn = document.getElementById("close--btn");
 
 function toggleButton() {
     navList.classList.toggle("show");
 }
 
+function closeButton() {
+    closeBtn.parentNode.classList.toggle("disabled");
+}
+
 humburger.addEventListener("click", toggleButton);
+closeBtn.addEventListener("click", closeButton);
